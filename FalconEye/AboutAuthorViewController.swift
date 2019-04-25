e46199f9bf95b367ab934cefc49432127cb02059
Kevin <castilloreyeskm@gmail.com>
2019-04-25 12:30:44 -0500

    Project Finished

---
+++ FalconEye/AboutAuthorViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AboutAuthorViewController.swift
+//  FalconEye
+//
+//  Created by Kevin Castillo Reyes on 2/25/19.
+//  Copyright © 2019 Kevin Castillo Reyes. All rights reserved.
+//
+
+import UIKit
+
+class AboutAuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func close(){
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ FalconEye/AboutViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AboutViewController.swift
+//  FalconEye
+//
+//  Created by Kevin Castillo Reyes on 2/18/19.
+//  Copyright © 2019 Kevin Castillo Reyes. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+    @IBAction func close(){
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
